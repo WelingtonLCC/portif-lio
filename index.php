3ec99568e571a031a8ca0ef07773ec41ad07aafe
WelingtonLuiz <welington.c.campos@hotmail.com>
2016-12-12 17:07:30 -0200

    inicio html header

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html >
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Desenvolvimento de páginas web, design, manutenção e softwares Wl frelancer em Santa Barbara D'Oeste </title>
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/design.css">
+  </head>
+  <body>
+    <!-- iniciando o menu deslizante  -->
+    <div class="conteiner">
+      <nav>
+        <div>
+          <ul class="left menu-principal">
+            <li><h3><a href="#">Home</a></h3></li>
+            <li><h3><a href="#">Noticias</a></h3></h3></li>
+            <li><h3><a href="#">Contato</a></h3></h3></li>
+            <li><h3><a href="#">Trabalhos</a></h3></h3></li>
+            <li><h3><a href="#">Design</a></h3></h3></li>
+          </ul>    
+       </div> 
+       <div class="espaco">
+         <ul class="left menu-principal">
+           <li class="imgUser"><img src="image/user-padrao.png" alt="Usúario"></li>
+           <li><h3 class="logar cursorDefault">Logar</h3></li>
+           <li><img class="pointer" src="image/seta-user.png" alt="Opções"></li>
+         </ul>
+       </div>
+      </nav>
+      <!-- Inicio do header independente do menu -->
+      <header class="imgBanner">
+        <!-- Imagem de fundo atribuido via Css -->
+        <div class="Container-header">
+          <div class="blocoTitulo">
+             <h1 class="cursorDefault">WL - Sistemas</h1>
+            <p class="cursorDefault">Página portifólio para divulgação de trabalhos realizados por mim Welington Luiz Correa Campos</p>  
+          </div>
+        </div>
+      </header>
+      <main>
+        <!-- Artigo Noticias -->
+        <article></article>
+        <!-- Artigo Trabalhos -->
+        <article></article>
+        <!-- Artigo contato -->
+        <article></article>
+      </main>
+      <footer>
+        <!-- Cartão Social/Apresentações do Desenvolvedor -->
+        <div class="container-footer">
+          <article>
+            <div class="menu-lateral">
+              <ul>
+                 <li class="gradient-menu-lateral"><h3><a href="#">Cartão Social</a></h3></li>
+                 <li><h3><a href="#">Localização</a></h3></li>
+                 <li><h3><a href="#">Competencias</a></h3></li>
+                 <li><h3><a href="#">Ferramentas</a></h3></li>
+                 <li><h3><a href="#">Serviços</a></h3></li>
+               </ul> 
+            </div>
+            <section>
+              
+            </section>
+          </article>
+          <!-- Direítos -->
+        </div>
+        <div class="address">
+          <p>Todos os Direítos Reservados₢ WLCC </p> 
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
